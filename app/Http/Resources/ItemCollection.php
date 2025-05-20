a9ad546505b6f4ab7fe838f2d02a17be1606a93d
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-20 12:33:10 +0700

    feat: add item collection

---
+++ app/Http/Resources/ItemCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ItemCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => ItemResource::collection($this->collection),
+        ];
+    }
+
+    public function with($request)
+    {
+        return [
+            'meta' => [
+                'total_items' => $this->collection->count(),
+            ],
+        ];
+    }
+}
