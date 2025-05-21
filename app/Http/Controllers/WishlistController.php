ddac8cf880f682bab3dd01b66182a952be9d1fa1
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-22 00:39:11 +0700

    chore: generate wishlist controller

---
+++ app/Http/Controllers/WishlistController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WishlistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // TODO: Implement index wishlist method
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // TODO: Implement store wishlist method
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $itemId)
+    {
+        // TODO: Implement destroy wishlist method
+    }
+}
