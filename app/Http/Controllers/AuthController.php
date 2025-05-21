d09d415df95858393bfbd45e62a9df1fa75cfb02
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-21 09:00:52 +0700

    feat: add user login endpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,26 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Hash;
 use Illuminate\Http\JsonResponse;
+use Throwable;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request): JsonResponse
     {
         $data = $request->validated();
-
         $user = new User($data);
+
         $user->password = Hash::make($data['password']);
+
         $user->save();
 
         return response()->json([
             'status' => true,
             'message' => 'Register account successfully.',
-            'token' => $user->createToken($user['username'])->plainTextToken,
+            'token' => $user->createToken($user->username)->plainTextToken,
             'data' => $user,
         ], 200);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        try {
+            $data = $request->validated();
+            $user = User::where('username', $data['username'])->first();
+
+            if (empty($user)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Username not found.',
+                ], 200);
+            }
+
+            if (!Hash::check($data['password'], $user->password)) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Password does not match.',
+                ], 401);
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'Login successfully.',
+                'token' => $user->createToken($user->username)->plainTextToken,
+                'data' => $user,
+            ], 200);
+        } catch (Throwable $e) {
+            return response()->json(
+                [
+                    'status' => false,
+                    'message' => $e->getMessage()
+                ],
+                500
+            );
+        }
     }
 }
