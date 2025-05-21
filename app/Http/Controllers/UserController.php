d511c62d7f896a3a77283851d00d0ef430081193
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-21 12:23:25 +0700

    refactor: add json repsone return type

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -26,7 +27,7 @@ class UserController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
         $user = User::get()->where("id", $id)->first();
         $user = User::find($id);
@@ -57,7 +58,7 @@ class UserController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
         $user = User::find($id);
 
