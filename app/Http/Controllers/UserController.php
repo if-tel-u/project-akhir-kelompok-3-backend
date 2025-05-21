6d7a3646e621f79c7196eb9132898ca0ed396067
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-21 12:18:25 +0700

    refactor: simplify user retrieval

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -29,6 +29,7 @@ class UserController extends Controller
     public function show(string $id)
     {
         $user = User::get()->where("id", $id)->first();
+        $user = User::find($id);
 
         if (empty($user)) {
             return response()->json([
