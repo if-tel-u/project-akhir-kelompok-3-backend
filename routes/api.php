7095f3b164dd9ebd84bb7f1457a65ae246a57367
Arief Rahman <140642047+ariefrahmanna@users.noreply.github.com>
2025-05-22 15:36:00 +0700

    chore: standardize route paths in API routes

--- routes/api.php
+++ routes/api.php
@@ -6,25 +6,25 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WishlistController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::delete('logout', [AuthController::class, 'logout']);
+    Route::delete('/logout', [AuthController::class, 'logout']);
 
-    Route::get('user', [UserController::class, 'index']);
-    Route::put('user', [UserController::class, 'update']);
-    Route::delete('user', [UserController::class, 'destroy']);
-    Route::get('users/{id}', [UserController::class, 'show']);
+    Route::get('/user', [UserController::class, 'index']);
+    Route::put('/user', [UserController::class, 'update']);
+    Route::delete('/user', [UserController::class, 'destroy']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
 
-    Route::get('items', [ItemController::class, 'index']);
-    Route::post('items', [ItemController::class, 'store']);
-    Route::get('items/{id}', [ItemController::class, 'show']);
-    Route::put('items/{id}', [ItemController::class, 'update']);
-    Route::delete('items/{id}', [ItemController::class, 'destroy']);
-    Route::get('user/items', [ItemController::class, 'userItems']);
+    Route::get('/items', [ItemController::class, 'index']);
+    Route::post('/items', [ItemController::class, 'store']);
+    Route::get('/items/{id}', [ItemController::class, 'show']);
+    Route::put('/items/{id}', [ItemController::class, 'update']);
+    Route::delete('/items/{id}', [ItemController::class, 'destroy']);
+    Route::get('/user/items', [ItemController::class, 'userItems']);
 
-    Route::get('wishlists', [WishlistController::class, 'index']);
-    Route::post('wishlists', [WishlistController::class, 'store']);
+    Route::get('/wishlists', [WishlistController::class, 'index']);
+    Route::post('/wishlists', [WishlistController::class, 'store']);
     Route::delete('/user/items/{itemId}', [WishlistController::class, 'destroy']);
 });
